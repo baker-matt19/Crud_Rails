de14e278e5ca397de5798765c8bb6b71eb4a500b
Matt Baker <matt.sheetz19@gmail.com>
2024-11-01 12:26:38 -0600

    Got comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,22 @@
 class CommentsController < ApplicationController
+  # used a rails migrate to create coulmn :post_id in comments table
+  def create
+    # finding the post by the id
+    @post = Post.find(params[:post_id])
+    # creating a new comment
+    @comment = @post.comments.create(params[:comment].permit(:name, :comment))
+    # redirecting to the post
+    redirect_to post_path(@post)
+  end
 
-	def create
-		@post = Post.find(params[:post_id])
-	 	@comment = @post.comments.create(params[:comment].permit(:name, :comment))
-		redirect_to post_path(@post)	
-	end
-
-	def destroy
-		@post = Post.find(params[:post_id])
-		@comment = @post.comments.find(params[:id])
-		@comment.destroy
-		redirect_to post_path(@post)
-	end
+  def destroy
+    # finding the post by the id
+    @post = Post.find(params[:post_id])
+    # finding the comment by the id
+    @comment = @post.comments.find(params[:id])
+    # deleting the comment
+    @comment.destroy
+    # redirecting to the post
+    redirect_to post_path(@post)
+  end
 end
